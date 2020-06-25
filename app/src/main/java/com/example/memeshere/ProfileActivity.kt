587c64d93281baf580a2a81a4906fec5c46ea774
radas <123radeth@gmail.com>
2020-06-25 18:47:36 +0200

    todo zmiana sposobu zapisu

--- app/src/main/java/com/example/memeshere/ProfileActivity.kt
+++ app/src/main/java/com/example/memeshere/ProfileActivity.kt
@@ -1,30 +1,58 @@
 package com.example.memeshere
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import com.example.memeshere.model.User
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.*
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
 import kotlinx.android.synthetic.main.activity_profile.*
+import java.io.File
+import java.util.*
 
 class ProfileActivity : AppCompatActivity() {
-    private lateinit var database: DatabaseReference
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
-        sign_out_button.setOnClickListener{
+
+        sign_out_button.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
         }
+
+        selectphoto_button_register.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 0)
+        }
+
         val uid = FirebaseAuth.getInstance().uid ?: ""
-        val ref = FirebaseDatabase.getInstance().getReference("/users/$uid/username")
-        val postListener = object : ValueEventListener {
+        val ref = FirebaseDatabase.getInstance().getReference("/users/$uid/")
+        val userListener = object : ValueEventListener {
+            @SuppressLint("SetTextI18n")
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 // Get Post object and use the values to update the UI
-                val nick = dataSnapshot.value.toString()
-                nickname.text = nick
-                // ...
+                val user: User? = dataSnapshot.getValue(User::class.java)
+                val nick = user?.username
+                nickname.text = "Hello $nick"
+                val file = Uri.fromFile(File(user?.profileImg))
+//                setImage(file)
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
@@ -33,10 +61,66 @@ class ProfileActivity : AppCompatActivity() {
                 // ...
             }
         }
-        ref.addValueEventListener(postListener)
+        ref.addValueEventListener(userListener)
+        val storage = Firebase.storage
+        val storageRef = storage.reference
+        val profileImgReference = storageRef.child("images/test.jpeg")
+        val ONE_MEGABYTE: Long = 1024 * 1024
+        profileImgReference.getBytes(ONE_MEGABYTE).addOnSuccessListener { bytes ->
+            val profileImgBitmap: Bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+            setImage(profileImgBitmap)
+        }
+    }
 
+    @RequiresApi(Build.VERSION_CODES.P)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
 
+        if (requestCode == 0 && resultCode == Activity.RESULT_OK && data != null) {
+            val selectedPhotoUri: Uri? = data.data
+            val source = ImageDecoder.createSource(contentResolver, selectedPhotoUri!!)
+            val bitmap = ImageDecoder.decodeBitmap(source)
+            setImage(bitmap)
+            saveImageToStorage(selectedPhotoUri)
+        }
+    }
 
+    @RequiresApi(Build.VERSION_CODES.P)
+    private fun setImage(bitmap: Bitmap) {
 
+        selectphoto_imageview_register.setImageBitmap(bitmap)
+
+        selectphoto_button_register.alpha = 0f
+    }
+
+    private fun saveImageToStorage(selectedPhotoUri: Uri?) {
+        // Todo przerobić zapisywanie scieżeki do storage na  taką val profileImgReference = storageRef.child("images/test.jpeg")
+        val filename = UUID.randomUUID().toString()
+        val ref = FirebaseStorage.getInstance().getReference("/images/$filename")
+
+        ref.putFile(selectedPhotoUri!!)
+            .addOnSuccessListener { it ->
+                ref.downloadUrl.addOnSuccessListener {
+                    assignImageToUser(it.toString())
+                }
+            }
+            .addOnFailureListener {
+
+            }
+    }
+
+    private fun assignImageToUser(fileLocation: String) {
+        val uid = FirebaseAuth.getInstance().uid ?: ""
+        val ref = FirebaseDatabase.getInstance().getReference("/users/$uid/profileImg")
+
+        ref.setValue(fileLocation)
+            .addOnSuccessListener {
+                Toast.makeText(this, "Successful to save image", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "Failed to save image: ${it.message}", Toast.LENGTH_SHORT)
+                    .show()
+            }
     }
 }
+
--- app/src/main/java/com/example/memeshere/auth/LoginActivity.kt
+++ app/src/main/java/com/example/memeshere/auth/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.memeshere.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.example.memeshere.ProfileActivity
 import com.example.memeshere.R
@@ -36,8 +37,9 @@ class LoginActivity : AppCompatActivity() {
         }
         FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
             .addOnCompleteListener {
-                if (it.isSuccessful) return@addOnCompleteListener
+                if (!it.isSuccessful) return@addOnCompleteListener
 
+                Log.d("Login", "Successfully logged in: ${it.result?.user?.uid}")
 
                 val intent = Intent(this, ProfileActivity::class.java)
                 intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
