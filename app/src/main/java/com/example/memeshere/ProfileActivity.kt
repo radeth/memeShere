df3c761deb8279d0bb752fee008103068d110dbf
radas <123radeth@gmail.com>
2020-07-13 20:25:48 +0200

    Dodaj obrazek

--- app/src/main/java/com/example/memeshere/ProfileActivity.kt
+++ app/src/main/java/com/example/memeshere/ProfileActivity.kt
@@ -21,13 +21,15 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
 import com.google.firebase.storage.ktx.storage
 import kotlinx.android.synthetic.main.activity_profile.*
-import java.io.File
+import java.io.ByteArrayOutputStream
 import java.util.*
 
+@RequiresApi(Build.VERSION_CODES.P)
 class ProfileActivity : AppCompatActivity() {
-    @RequiresApi(Build.VERSION_CODES.P)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
@@ -41,6 +43,11 @@ class ProfileActivity : AppCompatActivity() {
             intent.type = "image/*"
             startActivityForResult(intent, 0)
         }
+        add_image.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 1)
+        }
 
         val uid = FirebaseAuth.getInstance().uid ?: ""
         val ref = FirebaseDatabase.getInstance().getReference("/users/$uid/")
@@ -49,30 +56,21 @@ class ProfileActivity : AppCompatActivity() {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 // Get Post object and use the values to update the UI
                 val user: User? = dataSnapshot.getValue(User::class.java)
+
                 val nick = user?.username
                 nickname.text = "Hello $nick"
-                val file = Uri.fromFile(File(user?.profileImg))
-//                setImage(file)
+                user?.profileImg?.let { reciveImageFromFirbaseAndSetImage(it) }
             }
 
+
             override fun onCancelled(databaseError: DatabaseError) {
-                // Getting Post failed, log a message
                 Log.w("t", "loadPost:onCancelled", databaseError.toException())
-                // ...
             }
         }
         ref.addValueEventListener(userListener)
-        val storage = Firebase.storage
-        val storageRef = storage.reference
-        val profileImgReference = storageRef.child("images/test.jpeg")
-        val ONE_MEGABYTE: Long = 1024 * 1024
-        profileImgReference.getBytes(ONE_MEGABYTE).addOnSuccessListener { bytes ->
-            val profileImgBitmap: Bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
-            setImage(profileImgBitmap)
-        }
+
     }
 
-    @RequiresApi(Build.VERSION_CODES.P)
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
@@ -81,9 +79,32 @@ class ProfileActivity : AppCompatActivity() {
             val source = ImageDecoder.createSource(contentResolver, selectedPhotoUri!!)
             val bitmap = ImageDecoder.decodeBitmap(source)
             setImage(bitmap)
-            saveImageToStorage(selectedPhotoUri)
+            saveImageToStorage(bitmap)
+        }
+        if (requestCode == 0 && resultCode == Activity.RESULT_OK && data != null) {
+            val selectedPhotoUri: Uri? = data.data
+            val source = ImageDecoder.createSource(contentResolver, selectedPhotoUri!!)
+            val bitmap = ImageDecoder.decodeBitmap(source)
+            setImage(bitmap)
+            saveImageToStorage(bitmap)
         }
     }
+
+
+    @SuppressLint("ShowToast")
+    private fun reciveImageFromFirbaseAndSetImage(fileName: String) {
+        val storage = Firebase.storage
+        val profileImgReference =
+            storage.getReferenceFromUrl("gs://meme-shere.appspot.com/images").child(fileName)
+        val downloadSizeLimit: Long = 5242880
+        profileImgReference.getBytes(downloadSizeLimit).addOnSuccessListener { bytes ->
+            val profileImgBitmap: Bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+            setImage(profileImgBitmap)
+        }.addOnFailureListener {
+            Toast.makeText(this, "Failed to load image: ${it.message}", Toast.LENGTH_SHORT)
+        }
+    }
+
 
     @RequiresApi(Build.VERSION_CODES.P)
     private fun setImage(bitmap: Bitmap) {
@@ -93,34 +114,30 @@ class ProfileActivity : AppCompatActivity() {
         selectphoto_button_register.alpha = 0f
     }
 
-    private fun saveImageToStorage(selectedPhotoUri: Uri?) {
-        // Todo przerobić zapisywanie scieżeki do storage na  taką val profileImgReference = storageRef.child("images/test.jpeg")
+    private fun saveImageToStorage(bitmap: Bitmap) {
         val filename = UUID.randomUUID().toString()
-        val ref = FirebaseStorage.getInstance().getReference("/images/$filename")
-
-        ref.putFile(selectedPhotoUri!!)
-            .addOnSuccessListener { it ->
-                ref.downloadUrl.addOnSuccessListener {
-                    assignImageToUser(it.toString())
-                }
-            }
-            .addOnFailureListener {
-
-            }
+        val ref = FirebaseStorage.getInstance().reference
+        val imagesRef = ref.child("images").child(filename)
+        val baos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+        imagesRef.putBytes(data).addOnSuccessListener {
+            assignImageToUser(filename)
+        }.addOnFailureListener {
+            Toast.makeText(this, "Failed to save image: ${it.message}", Toast.LENGTH_SHORT)
+                .show()
+        }
     }
 
-    private fun assignImageToUser(fileLocation: String) {
+    private fun assignImageToUser(fileName: String) {
         val uid = FirebaseAuth.getInstance().uid ?: ""
-        val ref = FirebaseDatabase.getInstance().getReference("/users/$uid/profileImg")
-
-        ref.setValue(fileLocation)
-            .addOnSuccessListener {
-                Toast.makeText(this, "Successful to save image", Toast.LENGTH_SHORT).show()
-            }
-            .addOnFailureListener {
-                Toast.makeText(this, "Failed to save image: ${it.message}", Toast.LENGTH_SHORT)
-                    .show()
-            }
+        val ref = FirebaseDatabase.getInstance().reference
+        val usersRef = ref.child("users").child(uid).child("profileImg")
+        usersRef.setValue(fileName).addOnSuccessListener {
+            Toast.makeText(this, "Successful to save image", Toast.LENGTH_SHORT).show()
+        }.addOnFailureListener {
+            Toast.makeText(this, "Failed to save image: ${it.message}", Toast.LENGTH_SHORT).show()
+        }
     }
 }
 
